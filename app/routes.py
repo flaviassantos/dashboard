71d87a0c2c25e51a1665f6d9a4499618bf27a2d0
Flavia Santos <flavia.souza.santos@halfwave.com>
2020-02-12 07:55:47 +0100

    Small fixes in the login page

--- app/routes.py
+++ app/routes.py
@@ -6,15 +6,11 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Flavia'}
     posts = [
         {
             'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'body': 'Project about...'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
